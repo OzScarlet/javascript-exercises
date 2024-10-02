fd818f96687417bb11b6ac77f20bf549473f605f
OzScarlet <170483445+OzScarlet@users.noreply.github.com>
2024-10-02 08:50:20 +0800

    Perform exercise 4. includes and filter functions still unclear.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+//this function takes in two arguments, the array itself
+// and the rest operator with argument name of remove
+// which will be the bundle of element/s made into an array
+// to identify which element/s will be removed
+const removeFromArray = function(array, ...remove) {
+    //the return function will create a new array 
+    // 
+    return array.filter(val => !remove.includes(val))
 };
 
 // Do not edit below this line
