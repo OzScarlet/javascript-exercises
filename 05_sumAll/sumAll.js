9486397591aa56865ec42798d6a0758e49c3df8a
OzScarlet <170483445+OzScarlet@users.noreply.github.com>
2024-10-02 09:25:13 +0800

    Perform exercise 5. Was able to accomplish until test 4

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(startNumber, stopNumber) {
+    
+    let totalSum = 0;
+    if (startNumber < 0 || stopNumber < 0) {
+        return "ERROR";
+    }
+    else if (!Number.isInteger(startNumber) || !Number.isInteger(stopNumber)) {
+        return "ERROR";
+    }
+    else if (startNumber < stopNumber) {
+        for (i = startNumber; i <= stopNumber; i++) {
+            totalSum = totalSum + i;
+        }
+    }
+    else if (startNumber > stopNumber) {
+        for (i = startNumber; i >= stopNumber; i--) {
+            totalSum = totalSum + i;
+        }
+    }
+    
+    return totalSum;
+    
 };
 
 // Do not edit below this line
